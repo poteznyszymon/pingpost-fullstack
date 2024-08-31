45655545642cdb0d93fd91da9af96e8275406703
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-08-31 21:32:35 +0200

    added image handler in post editor

--- client/src/components/PostEditor.tsx
+++ client/src/components/PostEditor.tsx
@@ -1,15 +1,30 @@
-import { Image } from "lucide-react";
+import { Image, X } from "lucide-react";
 import LoadingButton from "./LoadingButton";
 import { Textarea } from "./ui/textarea";
 import UserAvatar from "./UserAvatar";
 import { useQuery } from "@tanstack/react-query";
 import { User } from "@/lib/types";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 const PostEditor = () => {
   const { data: user } = useQuery<User>({ queryKey: ["authUser"] });
   const [userInput, setUserInput] = useState("");
+  const [img, setImg] = useState<string | null>(null);
+  const imageRef = useRef<HTMLInputElement | null>(null);
+
+  const handleImageChane = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.result) {
+          setImg(reader.result as string);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <div className="bg-card p-5 rounded-2xl sticky top-[5.25rem] shadow-sm">
@@ -23,6 +38,18 @@ const PostEditor = () => {
           className="max-h-[20rem] rounded-2xl px-5 py-2 resize-none h-auto"
         />
       </div>
+      {img && (
+        <div className="h-64 my-5 ml-[4rem] rounded-2xl overflow-hidden relative group">
+          <img className="object-contain w-full h-full " src={img} />
+          <div
+            title="close image"
+            onClick={() => setImg(null)}
+            className="absolute top-3 right-3 size-7 hidden bg-primary group-hover:flex cursor-pointer items-center justify-center rounded-full"
+          >
+            <X className="size-4" />
+          </div>
+        </div>
+      )}
       <div className="flex justify-end pt-5 items-center gap-5 ">
         <label htmlFor="image-input">
           <div
@@ -30,7 +57,13 @@ const PostEditor = () => {
             title="Add photo"
           >
             <Image className="text-primary size-5" />
-            <input id="image-input" hidden type="file" />
+            <input
+              ref={imageRef}
+              onChange={(e) => handleImageChane(e)}
+              id="image-input"
+              hidden
+              type="file"
+            />
           </div>
         </label>
         <LoadingButton disabled={!userInput} loading={false}>
